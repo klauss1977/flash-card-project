a8e76df41df42046725e8b052724fc370e8e1f03
klauss1977 <klauss1977@yahoo.com>
2023-11-19 16:22:49 +0200

    add flip to back function

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from random import choice
 
 BACKGROUND_COLOR = "#B1DDC6"
 CARD_FRONT = "D:\Coding\Python\Angela\Projects\\flash-card-project\images\card_front.png"
+CARD_BACK = "D:\Coding\Python\Angela\Projects\\flash-card-project\images\card_back.png"
 WRONG_BTN_IMAGE = "D:\Coding\Python\Angela\Projects\\flash-card-project\images\wrong.png"
 RIGHT_BTN_IMAGE = "D:\Coding\Python\Angela\Projects\\flash-card-project\images\\right.png"
 DATA_FILE = "D:\Coding\Python\Angela\Projects\\flash-card-project\data\\french_words.csv"
@@ -13,16 +14,26 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 word_data_frame = pandas.read_csv(DATA_FILE)
 word_dict = word_data_frame.to_dict(orient="records")
+english_word=""
 
 
 def next_card():
-    new_french_word = choice(word_dict)['French']
-    canvas.itemconfigure(card_title, text="French")
-    canvas.itemconfigure(card_word, text=new_french_word)
-
+    global english_word
+    word_item = choice(word_dict)
+    french_word = word_item['French']
+    english_word = word_item["English"]
+    canvas.itemconfigure(card_title, text="French", fill='black')
+    canvas.itemconfigure(card_word, text=french_word, fill='black')
+    canvas.itemconfigure(canvas_image, image=card_front_img)
+    window.after(3000, flip_to_back)
+def flip_to_back():
+    canvas.itemconfigure(card_title, text="English", fill='white')
+    canvas.itemconfigure(card_word, text=english_word, fill='white')
+    canvas.itemconfigure(canvas_image, image=card_back_img)
 
 card_front_img = PhotoImage(file=CARD_FRONT)
-canvas.create_image(400, 263, image=card_front_img)
+card_back_img = PhotoImage(file=CARD_BACK)
+canvas_image = canvas.create_image(400, 263, image=card_front_img)
 card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
 card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 canvas.grid(row=0, column=0, columnspan=2)
@@ -33,4 +44,5 @@ right_image = PhotoImage(file=RIGHT_BTN_IMAGE)
 right_button = Button(image=right_image, highlightthickness=0, command=next_card)
 right_button.grid(row=1, column=1)
 next_card()
+window.after(3000, flip_to_back)
 window.mainloop()
