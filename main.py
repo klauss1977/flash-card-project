66b4abd2ef9eebdeefd70e7eb7ad8b7823f93280
klauss1977 <klauss1977@yahoo.com>
2023-11-20 11:56:18 +0200

    final commit

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import pandas
 from random import choice
 
 BACKGROUND_COLOR = "#B1DDC6"
-CARD_FRONT = "D:\Coding\Python\Angela\Projects\\flash-card-project\images\card_front.png"
-CARD_BACK = "D:\Coding\Python\Angela\Projects\\flash-card-project\images\card_back.png"
-WRONG_BTN_IMAGE = "D:\Coding\Python\Angela\Projects\\flash-card-project\images\wrong.png"
-RIGHT_BTN_IMAGE = "D:\Coding\Python\Angela\Projects\\flash-card-project\images\\right.png"
-DATA_FILE = "D:\Coding\Python\Angela\Projects\\flash-card-project\data\\french_words.csv"
-WORDS_TO_LEARN = "D:\Coding\Python\Angela\Projects\\flash-card-project\data\\words_to_learn.csv"
+CARD_FRONT = "D:\Personale\Coding\Python\Angela\Projects\\flash-card-project\images\card_front.png"
+CARD_BACK = "D:\Personale\Coding\Python\Angela\Projects\\flash-card-project\images\card_back.png"
+WRONG_BTN_IMAGE = "D:\Personale\Coding\Python\Angela\Projects\\flash-card-project\images\wrong.png"
+RIGHT_BTN_IMAGE = "D:\Personale\Coding\Python\Angela\Projects\\flash-card-project\images\\right.png"
+DATA_FILE = "D:\Personale\Coding\Python\Angela\Projects\\flash-card-project\data\\french_words.csv"
+WORDS_TO_LEARN = "D:\Personale\Coding\Python\Angela\Projects\\flash-card-project\data\\words_to_learn.csv"
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
@@ -29,12 +29,17 @@ def next_card():
     global chosen_word
     global flip_timer
     window.after_cancel(flip_timer)
-    chosen_word = choice(word_dict)
-    french_word = chosen_word['French']
-    canvas.itemconfigure(card_title, text="French", fill='black')
-    canvas.itemconfigure(card_word, text=french_word, fill='black')
-    canvas.itemconfigure(canvas_image, image=card_front_img)
-    flip_timer = window.after(3000, flip_to_back)
+    try:
+        chosen_word = choice(word_dict)
+    except IndexError:
+        canvas.itemconfigure(card_title, text="No more words to learn")
+        canvas.itemconfigure(card_word, text="Congratulations!")
+    else:
+        french_word = chosen_word['French']
+        canvas.itemconfigure(card_title, text="French", fill='black')
+        canvas.itemconfigure(card_word, text=french_word, fill='black')
+        canvas.itemconfigure(canvas_image, image=card_front_img)
+        flip_timer = window.after(3000, flip_to_back)
 def flip_to_back():
     english_word = chosen_word["English"]
     canvas.itemconfigure(card_title, text="English", fill='white')
@@ -43,12 +48,14 @@ def flip_to_back():
 
 def update_word_list():
     if len(word_dict)!=0:
-        next_card()
         word_dict.remove(chosen_word)
         df = pandas.DataFrame.from_records(word_dict)
         df.to_csv(WORDS_TO_LEARN, index=False)
-    else:
-        return
+        #try:
+        next_card()
+        # except IndexError:
+        #     canvas.itemconfigure(card_title, text="No more words to learn")
+        #     canvas.itemconfigure(card_word, text="Congratulations!")
 
 card_front_img = PhotoImage(file=CARD_FRONT)
 card_back_img = PhotoImage(file=CARD_BACK)
